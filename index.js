11fe3ab7727ad8091f8e18e71a46b46925dc6181
Jannis R <mail@jannisr.de>
2017-07-19 01:32:43 +0200

    read Wiki dump to revisions list

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 'use strict'
 
-// todo
+const createParser = require('xml-flow')
+const ndjson = require('ndjson')
+const zlib = require('zlib')
+
+const showError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+// namespaces
+const PAGES = 0
+
+const onPage = (page) => {
+	if (+page.ns !== PAGES) return
+
+	// todo: page.redirect
+	for (let revision of page.revision) {
+		console.error(page.id, revision.id)
+		sink.write(page.id, page.title, revision.id, revision.timestamp)
+	}
+}
+
+const parser = createParser(process.stdin, {useArray: createParser.ALWAYS})
+parser.on('error', showError)
+parser.on('tag:page', onPage)
+
+const sink = ndjson.stringify()
+sink
+.on('error', showError)
+.pipe(zlib.createGzip())
+.on('error', showError)
+.pipe(process.stdout)
+.on('error', showError)
