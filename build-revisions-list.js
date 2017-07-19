e1bcca64e2e4c81f7907ae282640c4ff6074b8d2
Jannis R <mail@jannisr.de>
2017-07-19 02:30:29 +0200

    CLI tool

---
+++ build-revisions-list.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict'
+
+const esc = require('ansi-escapes')
+const {isatty} = require('tty')
+const {stringify} = require('ndjson')
+
+const revisionsList = require('./lib/revisions-list')
+
+const showError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+let clear = '\n'
+if (isatty(process.stderr.fd) && !isatty(process.stdout.fd)) {
+	clear = esc.eraseLine + esc.cursorTo(0)
+}
+const report = (pages, revisions) => {
+	process.stderr.write(clear + pages + ' pages, ' + revisions + ' revisions')
+}
+
+revisionsList(process.stdin, report, showError)
+.pipe(stringify())
+.on('error', showError)
+.pipe(process.stdout)
+.on('error', showError)
